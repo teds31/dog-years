abb54662f763a011bb7446ef5a53fd0f63fe9793
TEDDY STANOWSKI <TSTANOWSKI31@GMAIL.COM>
2017-09-30 12:27:53 -0700

    create variables for dog year calculations

---
+++ calculations.js
@@ -0,0 +1,21 @@
+//age variable
+let myAge = 31;
+
+//two human years for dog year math
+let earlyYears = 2;
+
+//two human years * 10.5 for dog years
+earlyYears = earlyYears * 10.5;
+
+//account for two years from myAge
+let laterYears = myAge - 2;
+
+//dog years calculation
+let dogYears = laterYears * 4;
+
+//my age in dog years;
+let myAgeInDogYears = earlyYears + laterYears;
+
+//string variablef or name and change to lowercase
+let myName = 'Teddy';
+myName = myName.toLowerCase();
